93dbdb945bf699aaa391fbd8b82fc769f1b75df0
ischommer <ischommer@467b73ca-7a2a-4603-9d3b-597d59a354a9>
2008-11-02 20:12:39 +0000

    MINOR Updated master language tables
    git-svn-id: http://svn.silverstripe.com/open/modules/youtubeservice/trunk@65089 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ lang/en_US.php
@@ -0,0 +1,26 @@
+<?php
+
+global $lang;
+
+$lang['en_US']['YoutubeGallery']['PLURALNAME'] = array(
+	'Youtube Galleries',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['YoutubeGallery']['SINGULARNAME'] = array(
+	'Youtube Gallery',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+$lang['en_US']['YoutubeWidget']['PLURALNAME'] = array(
+	'Youtube Widgets',
+	50,
+	'Pural name of the object, used in dropdowns and to generally identify a collection of this object in the interface'
+);
+$lang['en_US']['YoutubeWidget']['SINGULARNAME'] = array(
+	'Youtube Widget',
+	50,
+	'Singular name of the object, used in dropdowns and to generally identify a single object in the interface'
+);
+
+?>
